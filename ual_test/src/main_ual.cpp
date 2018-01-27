6df2606405a33e4c6f17d925900e1781fd6b219f
Bardo91 <19Bardo91@gmail.com>
2018-01-27 21:38:26 +0100

    added help text and adaptable commands

--- ual_test/src/main_ual.cpp
+++ ual_test/src/main_ual.cpp
@@ -19,56 +19,49 @@ int main(int _argc, char **_argv) {
     while (!mUal.isReady() && ros::ok()) {
         sleep(1);
     }
-
-
     std::this_thread::sleep_for(std::chrono::seconds(1));
 
     geometry_msgs::PoseStamped pose;
     int cmd;
+	double value;
     while(ros::ok()){
-	std::cin >> cmd;
-	switch(cmd){
-	case 0:
-	    mUal.takeOff(3.0);
-	    break;
-	case 1:
-	    mUal.land(true);
-	    break;
-	case 2:
-	    pose = mUal.pose();
-	    pose.pose.position.x +=2;
-	    mUal.goToWaypoint(pose);
-	    break;
-	case 3:
-	    pose = mUal.pose();
-	    pose.pose.position.x -=2;
-	    mUal.goToWaypoint(pose);
-	    break;
-	case 4:
-	    pose = mUal.pose();
-	    pose.pose.position.y +=2;
-	    mUal.goToWaypoint(pose);
-	    break;
-	case 5:
-	    pose = mUal.pose();
-	    pose.pose.position.y -=2;
-	    mUal.goToWaypoint(pose);
-	    break;
-	case 6:
-	    pose = mUal.pose();
-	    pose.pose.position.z +=2;
-	    mUal.goToWaypoint(pose);
-	    break;
-	case 7:
-	    pose = mUal.pose();
-	    pose.pose.position.z -=2;
-	    mUal.goToWaypoint(pose);
-	    break;
-	default:
-	    std::cout << "unknown command" << std::endl;
-	}
+		std::cout << "Options: 0) takeoff; 1) land; 2) step X; 3) step Y; 4) step Z" << std::endl;
+		std::cin >> cmd;
+		switch(cmd){
+		case 0:
+			std::cout << "Desired take off altitude: "
+			std::cin >> value;
+			mUal.takeOff(value);
+			break;
+		case 1:
+			mUal.land(true);
+			break;
+		case 2:
+			std::cout << "Desired step in X: "
+			std::cin >> value;
+			pose = mUal.pose();
+			pose.pose.position.x +=value;
+			mUal.goToWaypoint(pose);
+			break;
+		case 3:
+			std::cout << "Desired step in Y: "
+			std::cin >> value;
+			pose = mUal.pose();
+			pose.pose.position.y +=value;
+			mUal.goToWaypoint(pose);
+			break;
+		case 4:
+			std::cout << "Desired step in Z: "
+			std::cin >> value;
+			pose = mUal.pose();
+			pose.pose.position.z +=value;
+			mUal.goToWaypoint(pose);
+			break;
+		default:
+			std::cout << "unknown command" << std::endl;
+		}
 
-	std::cout << "Finished command, wayting for next one" << std::endl;
+		std::cout << "Finished command, wayting for next one" << std::endl;
     }
     
 }
